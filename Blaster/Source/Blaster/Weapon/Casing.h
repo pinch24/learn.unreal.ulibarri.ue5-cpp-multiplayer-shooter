18dac1eee5c3a2529423ad830465babc5641d283
pinch24 <pinch24@gmail.com>
2023-06-12 19:30:52 +0900

    Shell Physics

--- Blaster/Source/Blaster/Weapon/Casing.h
+++ Blaster/Source/Blaster/Weapon/Casing.h
@@ -15,7 +15,16 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
 private:
 	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* CasingMesh;
+
+	UPROPERTY(EditAnywhere)
+	float ShellEjectionImpulse;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* ShellSound;
 };
